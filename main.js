68016a1beb9dd514f1abdd85f947d1b3d38c2669
jinjoo-jung <wjdwlswn23@naevr.com>
2023-07-31 14:01:19 +0900

    Update:js

--- main.js
+++ main.js
@@ -1,28 +1,5 @@
-// Array.from()
-// 유사 배열(Array-like)을 실제 배열로 반환
-
-const arraylike = {
-    0:'A',
-    1:'B',
-    2: 'C',
-    length:3
-}
-
-console.log(arraylike.constructor === Array)
-console.log(arraylike.constructor === Object)
-
-Array.from(arraylike).forEach(item => console.log(item))
-
-// Array.isArray()
-// 배열 데이터인지 확인
-
-const array = ['A', 'B', 'C']
-const arraylike = {
-    0:'A',
-    1:'B',
-    2:'C',
-    length:3
-}
-
-console.log(Array.isArray(array))  // true
-console.log(Array.isArray(arraylike)) // false
+import number, {str, arr, hello} from './module.js'
+console.log(number) //123
+console.log(str)  // ABC
+console.log(arr) // []
+console.log(hello) // f hello () {}
