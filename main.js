31782820f9256abd0698cddefbbd00a31ff1bacf
jinjoo-jung <wjdwlswn23@naevr.com>
2023-08-02 01:43:57 +0900

    fetch

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
 
 
-const getMovies = (movies , callback) => {
-    fetch(`https://www.omdapi.com/?apikey=7035c60c&s=${movieName}`)
-      .then(res => res.json())
-      .then(res => {
-        console.log(res)
-        callback
-        90
-      })
-}
 
-getMovies('frozen', () => {
-    console.log('겨울왕국!')
-})
 
-getMovies('avengers', () => {
-    console.log('어벤져스!')
-})
 
-getMovies('avatar', () => {
-    console.log('아바타!')
-    console.log('test')
-})
+
+
+
+
+
