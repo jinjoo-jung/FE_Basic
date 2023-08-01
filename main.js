865cf006fda3574337f4fb8aabeabc5f4e3fd24f
jinjoo-jung <wjdwlswn23@naevr.com>
2023-08-01 14:04:08 +0900

    update: 동기비동기

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-import number, {str, arr, hello} from './module.js'
-console.log(number) //123
-console.log(str)  // ABC
-console.log(arr) // []
-console.log(hello) // f hello () {}
+
+
+const getMovies = (movies , callback) => {
+    fetch(`https://www.omdapi.com/?apikey=7035c60c&s=${movieName}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        callback
+        90
+      })
+}
+
+getMovies('frozen', () => {
+    console.log('겨울왕국!')
+})
+
+getMovies('avengers', () => {
+    console.log('어벤져스!')
+})
+
+getMovies('avatar', () => {
+    console.log('아바타!')
+})
--- module.js
+++ module.js
@@ -1,7 +1,7 @@
 // 기본 내보내기
 export default 123
 
+// 이름 내보내기
 export const str = 'ABC'
 export const arr = []
-export const hello = () => {}
 export function hello() {}
