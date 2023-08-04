d777ed7aed1ab9137f30f689e487244e97369bc3
jinjoo-jung <wjdwlswn23@naver.com>
2023-08-04 19:51:46 +0900

    handler

--- main.js
+++ main.js
@@ -7,11 +7,12 @@
 const parentEl = document.querySelector('.parent')
 const handler = () => {
     console.log('Parent!')
-    console.log('test')
+    console.log('test')    
+
 }
 
 parentEl.addEventListener('click', handler, {
-    capture: true
+
 })
 parentEl.removeEventListener('click', handler)
 
