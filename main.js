b568ab84a1126b92bf2a2ff4f0d985c0af4bfd2e
jinjoo-jung <wjdwlswn23@naevr.com>
2023-08-04 18:55:20 +0900

    EventListner

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 
 
 
+
+
+const inputEl = document.querySelector('input')
+const childEl = document.querySelector('.child')
+
+inputEl.addEventListener('keydown', event => {
+    console.log(event.key)
+})
