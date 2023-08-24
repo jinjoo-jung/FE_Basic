869327c578b4b0b3e98952cef8191a5930538d43
jinjoo-jung <wjdwlswn23@naver.com>
2023-08-24 12:19:09 +0900

    frontend-basic

--- history/main.js
+++ history/main.js
@@ -1,46 +1,8 @@
-const page1 =
-  <section class = "page1">
-    <h1>Page 1</h1>
-  </section>
-const page2 = 
-  <section class= "page2">
-    <h1>Page 2</h1>
-  </section>
-const page3 = 
-  <section page3 = "page3">
-    <h1>Page 3</h1>
-  </section>
-const pageNotFound = 
-   <section>
-    <h1>404 Page Not Found!</h1>
-   </section>
+// regExp라는 변수를 만들어서 실제로 정규식을 할당하고
+// fox라는 단어를 찾고 있다.
 
-const pages = [
-    {path: '#page1', template:page1},
-    {path: '#page2', template:page2},
-    {path: '#page3', template:page3}
-]
-const appEl = document.querySelector('#app')
+const regExp = /fox/gi;
 
-const render = () => {
-    // console.log(history)
-    const page = pages.find(page => page.path === location.hash)
-    appEl.innerHTML = page
-       ? page.template
-       : pageNotFound
-}
-window.addEventListener('position', render)
-render()
-
-const pagePush = num => {
-    history.pushState(`전달할 데이터 - ${num}` , null , `#/page${num}`)
-    render()
-
-}
-
-const inputEl = document.querySelector('nav input')
-inputEl.addEventListener('keydown', event => {
-    if(event.key ==='Enter') {
-        pagePush(evnet.target.value)
-    }
-})
+console.log(regExp.test("fox"));
+console.log(str.matxh(regExp));
+console.log(str.replace(regExp));
--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+
+let user = {
+    name: 'Kim',
+    asge:20
+}
+function sayHi() {
+    alert(`나는 ${this.name}이야. 잘부탁해`)
+}
+
+user.sayHi = sayHi;
+user.sayHi();  // 나는 Kim이야. 잘부탁해
+
